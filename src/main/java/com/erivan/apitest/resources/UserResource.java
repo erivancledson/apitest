558f4ac1b3a79413f912c22a98b57b23ff2a4dcd
Erivan Cledson <erivancled@hotmail.com>
2021-11-28 09:35:00 -0300

    endpoint listar todos os usuários

--- src/main/java/com/erivan/apitest/resources/UserResource.java
+++ src/main/java/com/erivan/apitest/resources/UserResource.java
@@ -1,7 +1,8 @@
 package com.erivan.apitest.resources;
 
-import com.erivan.apitest.domain.User;
+import com.erivan.apitest.domain.dto.UserDTO;
 import com.erivan.apitest.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,15 +10,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class)); //pego o id que usuário informou e retorno o DTO
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll(){
+        //o x é o User que vai ser transformado em DTO
+        List<UserDTO> listDTO = service.findAll().stream().map(x-> mapper.map(x, UserDTO.class)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
--- src/main/java/com/erivan/apitest/services/UserService.java
+++ src/main/java/com/erivan/apitest/services/UserService.java
@@ -2,7 +2,10 @@ package com.erivan.apitest.services;
 
 import com.erivan.apitest.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
